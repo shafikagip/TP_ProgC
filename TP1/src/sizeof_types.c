fd89c6c926eafc384d491cdc26e8e43365b299f1
shafikagip <shafikagip@gmail.com>
2025-10-24 10:12:59 +0200

    Update sizeof_types.c

---
+++ TP1/src/sizeof_types.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(void) {
+
+    printf("Tailles des types de base en C (en octets) :\n\n");
+
+    printf("char : %zu octets\n", sizeof(char));
+    printf("signed char : %zu octets\n", sizeof(signed char));
+    printf("unsigned char : %zu octets\n\n", sizeof(unsigned char));
+
+    printf("int : %zu octets\n", sizeof(int));
+    printf("signed int : %zu octets\n", sizeof(signed int));
+    printf("unsigned int : %zu octets\n\n", sizeof(unsigned int));
+
+    printf("float : %zu octets\n", sizeof(float));
+  
+      return 0;
+
+
+
+
+
+
+}
