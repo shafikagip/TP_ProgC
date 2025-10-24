632f03a4396cc8b5784027a281fc450867d11b8c
shafikagip <shafikagip@gmail.com>
2025-10-24 11:10:32 +0200

    Update opérateurs.c

--- TP1/src/opérateurs.c
+++ TP1/src/opérateurs.c
@@ -2,9 +2,9 @@
 
 int main(void) {
 
-int a = 16
+int a = 16;
 
-int b = 3
+int b = 3;
     
     printf(" Opérateurs arithmétiques \n");
     printf("a = %d, b = %d\n\n", a, b);
@@ -23,6 +23,7 @@ int b = 3
     printf("!(a == b) : %d\n", !(a == b));
     return 0;
 }
+
 
 
 
