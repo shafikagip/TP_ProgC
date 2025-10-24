8db4f955e9d1aa32f10857611af6f1b95ea5fdd5
shafikagip <shafikagip@gmail.com>
2025-10-24 11:41:02 +0200

    Update opérateurs.c

--- TP1/src/opérateurs.c
+++ TP1/src/opérateurs.c
@@ -2,11 +2,10 @@
 
 int main(void) {
 
-int a = 16;
-
-int b = 3;
+    int a = 16;
+    int b = 3;
     
-    printf(" Opérateurs arithmétiques \n");
+    printf(" Opérateurs arithmétiques ");
     printf("a = %d, b = %d\n\n", a, b);
     printf("a + b = %d\n", a + b);
     printf("a - b = %d\n", a - b);
@@ -21,9 +20,7 @@ int b = 3;
     printf("(a > 10) && (b < 5) : %d\n", (a > 10) && (b < 5));
     printf("(a > 10) || (b > 5) : %d\n", (a > 10) || (b > 5));
     printf("!(a == b) : %d\n", !(a == b));
+
     return 0;
 }
-
-
-
 
