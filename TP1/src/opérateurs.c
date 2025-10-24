e8b94993239d4bc2ebb1e037cc6121ecce0e5595
shafikagip <shafikagip@gmail.com>
2025-10-24 11:09:45 +0200

    Update opérateurs.c
    ;

--- TP1/src/opérateurs.c
+++ TP1/src/opérateurs.c
@@ -20,8 +20,9 @@ int b = 3
     printf("a >= b : %d\n", a >= b);
     printf("(a > 10) && (b < 5) : %d\n", (a > 10) && (b < 5));
     printf("(a > 10) || (b > 5) : %d\n", (a > 10) || (b > 5));
-    printf("!(a == b) : %d\n", !(a == b))
+    printf("!(a == b) : %d\n", !(a == b));
     return 0;
 }
+
 
 
