58d30a70ddb999853e760d9a7775d2bbadfdb98f
shafikagip <shafikagip@gmail.com>
2025-10-24 10:24:47 +0200

    Update variables.c
    t

--- TP1/src/variables.c
+++ TP1/src/variables.c
@@ -5,9 +5,10 @@ int main(void) {
     char c = 'A';
     signed char sc = -65;
     unsigned char uc = 200;
-    printf("=== Affichage des variables ===\n\n");
+    printf(" Affichage des variables \n");
     printf("char : %c\n", c);
     printf("signed char : %d\n", sc);
     printf("unsigned char : %u\n\n", uc);
    return 0;
 }
+
