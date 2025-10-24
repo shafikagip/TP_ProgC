44ce2d5c237b00cfc4241827177c6c5fad796b9a
shafikagip <shafikagip@gmail.com>
2025-10-24 11:45:18 +0200

    Update boucles.c

---
+++ TP1/src/boucles.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main(void) {
+    int a = 16;
+    int b = 3;
+
+    printf("=== Opérateurs arithmétiques ===\n");
+    printf("a = %d, b = %d\n\n", a, b);
+
+    printf("Addition (a + b) = %d\n", a + b);
+    printf("Soustraction (a - b) = %d\n", a - b);
+    printf("Multiplication (a * b) = %d\n", a * b);
+    printf("Division entière (a / b) = %d\n", a / b);
+    printf("Modulo (a %% b) = %d\n\n", a % b);
+
+    printf("Opérateurs de comparaison \n");
+    printf("a == b : %d\n", a == b);
+    printf("a != b : %d\n", a != b);
+    printf("a > b  : %d\n", a > b);
+    printf("a < b  : %d\n", a < b);
+    printf("a >= b : %d\n", a >= b);
+    printf("a <= b : %d\n\n", a <= b);
+
+ 
+    printf(" Opérateurs logiques \n");
+    printf("(a > 10) && (b < 5) : %d\n", (a > 10) && (b < 5));
+    printf("(a > 10) || (b > 5) : %d\n", (a > 10) || (b > 5));
+    printf("!(a == b) : %d\n", !(a == b));
+
+    return 0;
+}
