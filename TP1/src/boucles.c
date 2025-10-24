6b22aaa78e036c094bf7feac74f37deb555b5b43
shafikagip <shafikagip@gmail.com>
2025-10-24 11:47:44 +0200

    Update boucles.c

--- TP1/src/boucles.c
+++ TP1/src/boucles.c
@@ -1,1 +1,23 @@
+#include <stdio.h>
+
+int main(void) {
+    int compteur = 5;
+    if (compteur >= 10) {
+        printf("Erreur : compteur doit être < 10\n");
+        return 1;
+    }
+    for (int i = 1; i <= compteur; i++) {
+        for (int j = 1; j <= i; j++) {
+            if (i > 2 && j < i) {
+                printf("# ");
+            } else {
+                printf("* ");
+            }
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
+
 
