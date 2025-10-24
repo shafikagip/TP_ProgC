397f33820a95d6b81c69b440e5bc2e56e00611aa
shafikagip <shafikagip@gmail.com>
2025-10-24 10:17:28 +0200

    Update sizeof_types.c
    ajt du short / long int / long long int

--- TP1/src/sizeof_types.c
+++ TP1/src/sizeof_types.c
@@ -12,7 +12,22 @@ int main(void) {
     printf("signed int : %zu octets\n", sizeof(signed int));
     printf("unsigned int : %zu octets\n\n", sizeof(unsigned int));
 
+    printf("short : %zu octets\n", sizeof(short));
+    printf("signed short : %zu octets\n", sizeof(signed short));
+    printf("unsigned short : %zu octets\n\n", sizeof(unsigned short));
+
+    printf("long int : %zu octets\n", sizeof(long int));
+    printf("signed long int : %zu octets\n", sizeof(signed long int));
+    printf("unsigned long int : %zu octets\n\n", sizeof(unsigned long int));
+
+    printf("long long int : %zu octets\n", sizeof(long long int));
+    printf("signed long long int : %zu octets\n", sizeof(signed long long int));
+    printf("unsigned long long int : %zu octets\n\n", sizeof(unsigned long long int));
+
+
     printf("float : %zu octets\n", sizeof(float));
+    printf("double : %zu octets\n", sizeof(double));
+    printf("long double : %zu octets\n", sizeof(long double));
   
       return 0;
 
