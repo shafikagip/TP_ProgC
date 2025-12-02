7f2a4a915e812ebe014f19d825d55eaea4e6b6cf
shafikagip <shafikagip@gmail.com>
2025-12-02 09:50:22 +0100

    Update calculs.c

--- TP1/src/calculs.c
+++ TP1/src/calculs.c
@@ -1,1 +1,32 @@
+#include <stdio.h>
+
+int main() {
+    int num1, num2;
+    char op;
+
+    scanf("%d", &num1);
+    scanf("%d", &num2);
+    scanf(" %c", &op);
+
+    switch (op) {
+        case '+': printf("%d\n", num1 + num2); break;
+        case '-': printf("%d\n", num1 - num2); break;
+        case '*': printf("%d\n", num1 * num2); break;
+        case '/':
+            if (num2 == 0) printf("Erreur\n");
+            else printf("%d\n", num1 / num2);
+            break;
+        case '%':
+            if (num2 == 0) printf("Erreur\n");
+            else printf("%d\n", num1 % num2);
+            break;
+        case '&': printf("%d\n", num1 & num2); break;
+        case '|': printf("%d\n", num1 | num2); break;
+        case '~': printf("%d\n", ~num1); break;
+        default: printf("Erreur\n");
+    }
+
+    return 0;
+}
+
 
